18120713aa101cb9138fe3650eacde48422eb323
GEstima <giannemayestima@gmail.com>
2024-04-16 11:13:55 +0800

    Create book factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => isset(static::$password) ? static::$password : Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
